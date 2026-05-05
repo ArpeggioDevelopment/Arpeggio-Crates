ba2a0e27691f819a69f1b7e6ff04c758f64bef88
BlavezDev <blavez@proton.me>
2026-05-05 19:51:35 +0200

    setup paper dependency

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -7,8 +7,13 @@ version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
 
 }
